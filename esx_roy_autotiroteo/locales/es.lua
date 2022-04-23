3500899d291ae02cc410605bfd0419071b037fb6
carlos837 <64087088+carlos837@users.noreply.github.com>
2022-04-23 16:47:11 +0000

    Add files via upload

---
+++ esx_roy_autotiroteo/locales/es.lua
@@ -0,0 +1,12 @@
+
+Locales['es'] = {
+    ['msg1'] = "Entornos de Tiroteos",
+    ['msg2'] = "Tiroteo Pistolas",
+    ['msg3'] = "Tiroteo Subfusiles",
+    ['msg4'] = "Tiroteo Fusiles de Asalto",
+    ['msg5'] = "Tiroteo de Escopetas",
+    ['msg6'] = "Tiroteo de Machinengun",
+    ['msg7'] = "Tiroteo de Carabina",
+    ['msg8'] = "Tiroteo de Granada",
+}
+    
